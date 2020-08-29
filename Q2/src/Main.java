971577f7aa635976d26d3f5d2fb346f8682e8005
Administrator <Administrator@sumedha-PC>
2020-08-29 12:37:12 +0530

    new line is added

--- Q2/src/Main.java
+++ Q2/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter first string:");
 		String first=scanner.nextLine();
