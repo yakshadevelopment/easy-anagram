2a9998a7f35f06f4a1dbc4f8d5c8b12bb88ac1ad
Administrator <Administrator@sumedha-PC>
2020-08-29 12:29:15 +0530

    THis is easy queston for java8

---
+++ Q2/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter first string:");
+		String first=scanner.nextLine();
+		System.out.println("Enter second string:");
+		String second=scanner.nextLine();
+		Anagram anagram=new Anagram(first,second);
+		if(anagram.checkAnagram())
+			System.out.println("They are anagrams");
+		else
+		  System.out.println("They are not anagram");
+
+	}
+
+}
